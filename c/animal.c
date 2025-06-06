c2847936e33fa0d9d201df3b4fb6c03f350c9ab5
Detlef Groth <75636+mittelmark@users.noreply.github.com>
2025-06-06 09:29:12 +0200

    adding c exampple using void pointers

---
+++ c/animal.c
@@ -0,0 +1,77 @@
+// OOP with C
+// https://www.perplexity.ai/search/how-can-i-simulate-a-animal-cl-wqtfh.F0S_OT9PL2Houzsw
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Typedef for read function pointer - methods of our object
+typedef void (*read_func)(void* self);
+typedef void (*eat_func)(void* self,float amount);
+typedef void (*run_func)(void* self,float distance);
+// Animal struct definition
+struct Animal {
+    char* species;
+    int age;
+    char* name;
+    float weight;
+    read_func read;
+    eat_func eat;    
+    run_func run;
+} ;
+
+// Read method implementation
+void animal_read(void* self) {
+    struct Animal* animal = (struct Animal*)self;
+    printf("Species: %s\nAge: %d\nName: %s\n", 
+           animal->species, animal->age, animal->name);
+}
+void animal_eat(void* self,float amount) {
+    struct Animal* animal = (struct Animal*)self;
+    animal->weight += amount;
+    printf("Weight of %s after eat: %0.1f\n",animal->name,animal->weight);
+}
+
+void animal_run(void* self,float distance) {
+    struct Animal* animal = (struct Animal*)self;
+    animal->weight -= distance*0.1;
+    printf("%s's weight after running %0.2fkm: %0.2f\n",animal->name,distance,animal->weight);
+}
+
+// Constructor
+struct Animal* Animal_new(const char* species, int age, const char* name,float weight) {
+    struct Animal* obj = malloc(sizeof(struct Animal));
+    obj->species = strdup(species);
+    obj->age = age;
+    obj->name = strdup(name);
+    obj->weight = weight;
+    obj->read = animal_read;
+    obj->eat = animal_eat;
+    obj->run = animal_run;    
+    return obj;
+}
+
+// Destructor
+void Animal_delete(struct Animal** obj) {
+    free((*obj)->species);
+    free((*obj)->name);
+    free(*obj);
+    *obj = NULL;
+}
+#define ANIMAL(x,species,age,name,weight) struct Animal* x = Animal_new(species,age,name,weight);
+// Usage example
+int main() {
+    struct Animal* lion = Animal_new("Panthera leo", 5, "Simba",100.0);
+    struct Animal* micky = Animal_new("Cat", 18, "Micky",4.5);
+    ANIMAL(susi,"Cat",10,"Susi",5.2);
+    lion->eat(lion,4.5); // Explicit context passing
+    micky->eat(micky,0.1); 
+    $susi->eat(susi,0.1);
+    lion->run(lion,3.0); 
+    lion->read(lion);   
+    micky->run(micky,0.01);     
+    micky->read(micky);       
+    Animal_delete(&lion);
+    Animal_delete(&micky);    
+    return 0;
+}
+
