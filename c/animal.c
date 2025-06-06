2d618929b2f40fa8e42206649ef0a3d1b99e1cc0
Detlef Groth <75636+mittelmark@users.noreply.github.com>
2025-06-06 10:23:19 +0200

    adding delete macro

--- c/animal.c
+++ c/animal.c
@@ -47,17 +47,20 @@ struct Animal* Animal_new(const char* species, int age, const char* name,float w
     obj->read = animal_read;
     obj->eat = animal_eat;
     obj->run = animal_run;    
+    printf("Animal %s created!\n",obj->name);
     return obj;
 }
 
 // Destructor
 void Animal_delete(struct Animal** obj) {
+    printf("Animal %s will be deleted!\n",(*obj)->name);
     free((*obj)->species);
     free((*obj)->name);
     free(*obj);
     *obj = NULL;
 }
 #define ANIMAL(x,species,age,name,weight) struct Animal* x = Animal_new(species,age,name,weight);
+#define DELETE(x)     Animal_delete(&x);
 // Usage example
 int main() {
     struct Animal* lion = Animal_new("Panthera leo", 5, "Simba",100.0);
@@ -65,13 +68,16 @@ int main() {
     ANIMAL(susi,"Cat",10,"Susi",5.2);
     lion->eat(lion,4.5); // Explicit context passing
     micky->eat(micky,0.1); 
-    $susi->eat(susi,0.1);
+    susi->eat(susi,0.1);
+    susi->run(susi,1);
     lion->run(lion,3.0); 
     lion->read(lion);   
     micky->run(micky,0.01);     
     micky->read(micky);       
+    susi->read(susi);
     Animal_delete(&lion);
-    Animal_delete(&micky);    
+    Animal_delete(&micky);  
+    DELETE(susi);
     return 0;
 }
 
